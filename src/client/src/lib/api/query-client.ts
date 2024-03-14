6bfa57b520788fb1b8f85eb0c9137236421aa5a0
djek-sweng <djek@gmx.ch>
2024-03-14 15:12:19 +0100

    react

--- src/client/src/lib/api/query-client.ts
+++ src/client/src/lib/api/query-client.ts
@@ -1,5 +1,15 @@
-import { QueryClient } from '@tanstack/react-query';
+import { QueryClient, useMutation } from '@tanstack/react-query';
+
+import { signin } from './http-client';
+import { setToken } from '../misc/local-store';
 
 const queryClient = new QueryClient();
 
 export default queryClient;
+
+export function useSignin() {
+  return useMutation({
+    mutationFn: signin,
+    onSuccess: (token) => setToken(token),
+  });
+}
--- src/client/src/pages/Signin.tsx
+++ src/client/src/pages/Signin.tsx
@@ -1,24 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
-
 import SigninForm from '../components/SigninForm';
 import PageContent from './../components/PageContent';
-import { signin } from './../lib/api/http-client';
-import { SigninDto, TokenDto } from '../lib/api/dtos';
-import { setToken } from '../lib/misc/local-store';
+
+import { useSignin } from '../lib/api/query-client';
 
 export default function Signin() {
-  const { mutate, isError, error } = useMutation({
-    mutationFn: signin,
-    onSuccess: (token) => handleSucces(token),
-  });
-
-  const handleSubmit = (dto: SigninDto) => mutate(dto);
-
-  const handleSucces = (token: TokenDto) => setToken(token);
+  const { mutate, isError, error } = useSignin();
 
   return (
     <PageContent title="Signin">
-      <SigninForm onSubmit={handleSubmit} />
+      <SigninForm onSubmit={mutate} />
       {isError && <p>{error.message}</p>}
     </PageContent>
   );

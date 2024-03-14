dedfdae8f1d884073aa943b13d98146c53487e42
djek-sweng <djek@gmx.ch>
2024-03-14 14:44:10 +0100

    react

--- src/client/src/lib/api/http-client.ts
+++ src/client/src/lib/api/http-client.ts
@@ -1,5 +1,5 @@
 import HttpClientError from './http-client-error';
-import { SignupDto, TokenDto } from './dtos';
+import { SignupDto, SigninDto, TokenDto } from './dtos';
 
 const _BASE_URL = 'http://localhost:5000';
 
@@ -14,6 +14,22 @@ export async function signup(dto: SignupDto): Promise<TokenDto> {
 
   if (!response.ok) {
     throw new HttpClientError(response, `Signup failed! ${data.message}`);
+  }
+
+  return data;
+}
+
+export async function signin(dto: SigninDto): Promise<TokenDto> {
+  const response = await fetch(`${_BASE_URL}/auth/signin`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(dto),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new HttpClientError(response, `Signin failed! ${data.message}`);
   }
 
   return data;

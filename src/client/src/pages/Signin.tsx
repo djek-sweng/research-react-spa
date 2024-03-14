9c9abce186774d7e480ff9ac3822c135f5afba59
djek-sweng <djek@gmx.ch>
2024-03-14 14:57:05 +0100

    react

--- src/client/src/pages/Signin.tsx
+++ src/client/src/pages/Signin.tsx
@@ -1,5 +1,25 @@
-export default function Signin() {
-  throw new Error('Signin page is not implemented.');
+import { useMutation } from '@tanstack/react-query';
 
-  return <></>;
+import SigninForm from '../components/SigninForm';
+import PageContent from './../components/PageContent';
+import { signin } from './../lib/api/http-client';
+import { SigninDto, TokenDto } from '../lib/api/dtos';
+import { setToken } from '../lib/misc/local-store';
+
+export default function Signin() {
+  const { mutate, isError, error } = useMutation({
+    mutationFn: signin,
+    onSuccess: (token) => handleSucces(token),
+  });
+
+  const handleSubmit = (dto: SigninDto) => mutate(dto);
+
+  const handleSucces = (token: TokenDto) => setToken(token);
+
+  return (
+    <PageContent title="Signin">
+      <SigninForm onSubmit={handleSubmit} />
+      {isError && <p>{error.message}</p>}
+    </PageContent>
+  );
 }

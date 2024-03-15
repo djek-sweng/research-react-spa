93308dfddea3cc1eba82546d2f1637c9810d3ded
djek-sweng <djek@gmx.ch>
2024-03-15 09:06:38 +0100

    react

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -2,15 +2,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 
 import RootLayout from './layouts/Root';
+import NotesLayout from './layouts/Notes';
 
 import ErrorPage from './pages/Error';
 import HomePage from './pages/Home';
 import SignupPage from './pages/Signup';
 import SigninPage from './pages/Signin';
 import SignoutPage from './pages/Signout';
+import NotesPage from './pages/Notes';
 
 import queryClient from './lib/api/query-client';
 import rootLoader, { ROOT_LOADER_ID } from './lib/misc/root-loader';
+import notesLoader, { NOTES_LOADER_ID } from './lib/misc/notes-loader';
 
 const router = createBrowserRouter([
   {
@@ -25,6 +28,18 @@ const router = createBrowserRouter([
       { path: 'signup', element: <SignupPage /> },
       { path: 'signin', element: <SigninPage /> },
       { path: 'signout', element: <SignoutPage /> },
+      {
+        path: 'notes',
+        element: <NotesLayout />,
+        children: [
+          {
+            index: true,
+            element: <NotesPage />,
+            id: NOTES_LOADER_ID,
+            loader: notesLoader,
+          },
+        ],
+      },
     ],
   },
 ]);

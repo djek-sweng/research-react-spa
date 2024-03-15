28c26e818b1cd3f4e02e0935130933a0e07247c9
djek-sweng <djek@gmx.ch>
2024-03-15 12:00:57 +0100

    react

--- src/client/src/pages/Notes.tsx
+++ src/client/src/pages/Notes.tsx
@@ -1,6 +1,7 @@
 import PageContent from '../components/PageContent';
 import NotesList from '../components/NotesList';
 import ErrorBlock from '../components/ErrorBlock';
+import LoadingIndicator from '../components/LoadingIndicator';
 import { useLoadNotes } from '../lib/api/query-client';
 
 function NotesPage() {
@@ -9,7 +10,7 @@ function NotesPage() {
   let content = <p>Please create notes.</p>;
 
   if (isLoading) {
-    content = <p>Loading...</p>;
+    content = <LoadingIndicator />;
   }
 
   if (isError) {
